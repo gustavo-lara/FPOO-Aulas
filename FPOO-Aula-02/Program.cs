ebc57f390beec39bc3c73a478cc2c187404c696a
Gustavo Lara <gustavolara63719@gmail.com>
2026-03-21 08:49:28 -0300

    commit

--- FPOO-Aula-02/Program.cs
+++ FPOO-Aula-02/Program.cs
@@ -9,8 +9,22 @@
     }
 
     //PILARES DO POO--- 
-    //Abstração: Trazer algo de fora do mundo real para o mundo da programação. Exemplo: Ler o nome do usuário e exibir uma mensagem personalizada.
-    //Herança: Criar uma nova classe que herda as características de uma classe existente. Exemplo: Criar uma classe "Pessoa" e depois criar uma classe "Aluno" que herda de "Pessoa".
-    //Encapsualemento: Esconder os detalhes internos de uma classe e fornecer uma interface pública para interagir com ela. Exemplo: Criar uma classe "ContaBancaria" que tem um método público "Depositar" e um campo privado "saldo".
-    //Polimorfismo: Permitir que objetos de diferentes classes sejam tratados como objetos da mesma classe base. Exemplo: Criar uma classe base "Animal" e depois criar classes derivadas "Cachorro" e "Gato" que implementam um método "FazerSom" de maneira diferente.
+    //Abstração: Trazer algo de fora do mundo real para o mundo da programação. 
+    //Exemplo: Ler o nome do usuário e exibir uma mensagem personalizada.
+
+    //Herança: Criar uma nova classe que herda as características de uma classe existente. 
+    //Exemplo: Criar uma classe "Pessoa" e depois criar uma classe "Aluno" que herda de "Pessoa".
+
+    //Encapsualemento: Esconder os detalhes internos de uma classe e fornecer uma interface pública para interagir com ela. 
+    //Exemplo: Criar uma classe "ContaBancaria" que tem um método público "Depositar" e um campo privado "saldo".
+
+    //Polimorfismo: Permitir que objetos de diferentes classes sejam tratados como objetos da mesma classe base. 
+    //Exemplo: Criar uma classe base "Animal" e depois criar classes derivadas "Cachorro" e "Gato" que implementam um método "FazerSom" de maneira diferente.
+
+    //Conceitos de POO---
+    //Classe: Define o que o objeto terá -> Atributos (características) e Métodos (ações) 
+    //Exemplo: Criar uma classe "Pessoa" com atributos "nome" e "idade" e métodos "falar" e "andar".
+
+    //Objeto: Quando a classe ganha vida na memória do computador
+    //Exemplo: Criar um objeto "pessoa1" da classe "Pessoa" e atribuir valores aos seus atributos (nome, idade) e chamar seus métodos (falar, andar).
 }
